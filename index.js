0cbd2e7bf31415a3925d9396485180ec193baabf
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-05-05 21:26:53 -0700

    stopping for the night, we do have some of our mungedLocation

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+//console.log('hello world');
+
 // const require = require('requirejs');
 const express = require('express'); 
 const dotenv = require('dotenv');
 dotenv.config();
-const cors = require('cor'); 
+const cors = require('cors'); 
 const app = express(); //this is how we get an app 
+app.use(cors());
+
 
 // import our data 
 const weatherdata = require('./data/weather.json');
@@ -13,62 +17,40 @@ const geodata = require('./data/geo.json');
 const PORT = process.env.PORT || 3001; 
 
 // Function that munges our data to fit what is in trello example 
-const oneCity = {
-    'place_id': '282983083',
-    'licence': 'https://locationiq.com/attribution',
-    'osm_type': 'relation',
-    'osm_id': '186579',
-    'boundingbox': [
-        '45.432536',
-        '45.6528812',
-        '-122.8367489',
-        '-122.4720252'
-    ],
-    'lat': '45.5202471',
-    'lon': '-122.6741949',
-    'display_name': 'Portland, Multnomah County, Oregon, USA',
-    'class': 'place',
-    'type': 'city',
-    'importance': 0.75356571743377,
-    'icon': 'https://locationiq.org/static/images/mapicons/poi_place_city.p.20.png'
-}; 
 
-
+const oneCity = "Portland, Multnomah County, Oregon, USA"; 
 
 function mungedLocation(oneCity) {
     // munge that data
     //once we have a single city, we want to just grab lat, lon and display_name
-    const test = (({ lat, lon }) => ({ lat, lon }))(oneCity);
 
-    console.log(test);
-    const mungedCity = { 
-        lat: '45.5202471',
-        lon: '-122.6741949',
-        display_name: 'Portland, Multnomah County, Oregon, USA',
-    }; 
+    const cityData = geodata.filter(a => a.display_name === oneCity)[0];
+    const cityLocation = (({ lat, lon, display_name }) => ({ lat, lon, display_name }))(cityData);
+
+    console.log(cityData);
     
+    console.log(cityLocation);
 
-    return mungedLocationData;
+    return cityLocation;
 }
-
-
-
 
 // create a route method of get and location 
 app.get('/location', (req, res) => {
+    // console.log('hello world');
     try {
-        const mungedResponse = mungedLocation(data);
+        const mungedResponse = mungedLocation(req);
         res.json(mungedResponse);
     } catch(e) {
+// console.error(e);
 
-        // console.error(e);
-
+        
         res.json({
             status: 500, 
             responseText: e,
         });
     }
 });
+
     
 
 
